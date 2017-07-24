24ed340e1bf0f429a60c73ed671465b23158caed
Albert Yu <ayu@atlassian.com>
2017-07-23 23:04:16 -0700

    add test and release step

--- chrome_build.sh
+++ chrome_build.sh
@@ -9,4 +9,5 @@ git reset --hard && git checkout master && git reset --hard && git rebase-update
 git fetch --verbose && (git show-ref --tags | grep refs | cut -d/ -f3 | sort -rn | head -1 > target.txt) && cat target.txt
 export TARGET=`cat target.txt` && git checkout $TARGET && git reset --hard && gclient sync --jobs 16 && mkdir -p out/$TARGET && cp ~/args.gn out/$TARGET; gn gen out/$TARGET
 export TARGET=`cat target.txt` && timeout 45m ninja -C out/$TARGET headless_shell || true
-export TARGET=`cat target.txt` && cd out/$TARGET && mkdir -p bin && cp ~/Dockerfile *.pak headless_shell bin && sudo docker build -t yukinying/chrome-headless:$TARGET -t yukinying/chrome-headless:latest bin && sudo docker push yukinying/chrome-headless:$TARGET && sudo docker push yukinying/chrome-headless:latest
+export TARGET=`cat target.txt` && cd out/$TARGET && mkdir -p bin && cp ~/Dockerfile *.pak headless_shell bin && sudo docker build -t yukinying/chrome-headless:$TARGET -t yukinying/chrome-headless:latest bin && sudo docker run --init -it --rm --entrypoint="/chrome/headless_shell" --name headless yukinying/chrome-headless:$TARGET --no-sandbox --disable-gpu "https://www.google.com" && sudo docker push yukinying/chrome-headless:$TARGET && sudo docker push yukinying/chrome-headless:latest
+export TARGET=`cat target.txt` && cd out/$TARGET && ~/chrome-release-to-github.sh
