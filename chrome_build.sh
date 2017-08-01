f4fb39dfad371f182377100b6ab857086f448039
Albert Yu <yukinying@users.noreply.github.com>
2017-07-31 17:33:55 -0700

    remove pseudo-TTY from test step

--- chrome_build.sh
+++ chrome_build.sh
@@ -9,6 +9,6 @@
 export TARGET=`cat target.txt` && git fetch origin $TARGET --depth=1 
 export TARGET=`cat target.txt` && git checkout $TARGET && gclient sync --no-history --with_branch_heads --with_tags --reset --shallow --jobs 16 && mkdir -p out/$TARGET && cp ~/args.gn out/$TARGET; gn gen out/$TARGET
 export TARGET=`cat target.txt` && timeout 45m ninja -C out/$TARGET headless_shell || true
-export TARGET=`cat target.txt` && cd out/$TARGET && mkdir -p bin && cp ~/Dockerfile *.pak headless_shell bin && sudo docker build -t yukinying/chrome-headless:$TARGET -t yukinying/chrome-headless:latest bin && sudo docker run --init -it --rm --entrypoint="/chrome/headless_shell" --name headless yukinying/chrome-headless:$TARGET --no-sandbox --disable-gpu "https://www.google.com" && sudo docker push yukinying/chrome-headless:$TARGET && sudo docker push yukinying/chrome-headless:latest
+export TARGET=`cat target.txt` && cd out/$TARGET && mkdir -p bin && cp ~/Dockerfile *.pak headless_shell bin && sudo docker build -t yukinying/chrome-headless:$TARGET -t yukinying/chrome-headless:latest bin && sudo docker run --init --rm --entrypoint="/chrome/headless_shell" --name headless yukinying/chrome-headless:$TARGET --no-sandbox --disable-gpu "https://www.google.com" && sudo docker push yukinying/chrome-headless:$TARGET && sudo docker push yukinying/chrome-headless:latest
 export TARGET=`cat target.txt` && cd out/$TARGET && ~/chrome-release-to-github.sh
 export TARGET=`cat target.txt` && cd out/$TARGET && ~/npm-publish.sh
